16df4b21fec069857dfd72abb5e7eb325eb82362
Tristanwyatt <tristan7306@gmail.com>
2020-08-28 14:42:48 -0500

    advanced

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -3,3 +3,17 @@ let joke;
 // 1. define and export a useJoke function that simply returns the joke object.
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
+
+export const useJoke = () => {
+    return joke;
+}
+
+export const getJoke = () =>{
+    return fetch('https://official-joke-api.appspot.com/jokes/random')
+    .then(response => response.json())
+    .then(response => joke = response);
+};
+
+
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
+import { getJoke, useJoke } from './JokeProvider.js';
+import { Joke } from './Joke.js';
+
+const punchBtn = document.querySelector(".request-punch-btn");
 const jokeBtn = document.querySelector(".request-joke-btn");
+const jokeSetup = document.querySelector(".joke-setup");
+const jokePunch = document.querySelector(".joke-punchline");
+
+
+
+let joke = "";
 
 jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
-  // TODO: fetch a joke from the Joke API and render it to the DOM
+  getJoke().then(() => {
+    joke = useJoke()
+    jokeSetup.innerHTML = `Setup: ${Joke(joke.setup)}`;
+  })
+
+
+
+punchBtn.addEventListener("click", (e) => {
+  // getJoke().then(() => {
+    joke = useJoke()
+    jokePunch.innerHTML = `Punch: ${Joke(joke.punchline)}`;
+  })
 });
+
+
+
+
+
+// TODO: fetch a joke from the Joke API and render it to the DOM
